97d01ae113678e7ded025e5b16e0b65f39385035
martindurantdf <104039754+martindurantdf@users.noreply.github.com>
2022-05-27 13:15:12 -0300

    Agregue enunciado

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
+/**
+ • El jardín infantil necesita un programa para poder asignar cursos a las aulas
+• Se cuentan con tres aulas: Azul, Verde y Amarilla
+• Cada aula cuenta con una capacidad diferente (es decir, un número de
+bancos)
+• La aula Azul tiene 40 bancos, la Verde 35 y la Amarilla 30
+• Dado un número de infantes ingresado por el usuario, el programa deberá
+determinar el aula que minimice la cantidad de bancos vacíos
+• La salida del algoritmo es el color que identifica al aula asignada
+ */
+
 let jardinSalaColor: string[] = ["Azul", "Verde", "Amarillo"];
 let jardinSalaCapacidad: number[] = [40, 35, 30];
 let sillas: number = 0;
